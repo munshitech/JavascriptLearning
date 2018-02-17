311f3616a084093960b864835132db01000d81f6
Anas Munshi <munshianas@gmail.com>
2018-02-17 10:18:35 -0600

    Intial

---
+++ JavaScriptDesignPatterns/Behavioral Patterns/task.js
@@ -0,0 +1,18 @@
+﻿var Module = require('./Module');
+var Task = function (data) {
+    this.name = data.name;
+    this.complete = false;
+}
+
+Task.prototype.completed = function () {
+    console.log("The task completed " + this.name);
+    complete = true;
+};
+
+Task.prototype.save = function () {
+    console.log("Saving task " + this.name);
+    Module.save(this);
+};
+
+
+module.exports = Task;
